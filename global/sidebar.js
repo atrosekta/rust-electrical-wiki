4f494543dc21998771fd60ae434cb4c5b37bf4fb
atrosekta <atrosekta@proton.me>
2023-04-12 06:48:05 +0000

    added css and js files,
    should have added all the previous versions for history
    but there was too much dead code from all the debuging
    what changed from the past version :
      - removed all the commented css and js
      - css file splited into 3 differents ones
      - small css changes evrywhere to make evrything cleaner
      - added main-container to fix mobile horizontal scroll on anchor click
      - cleaned and improved a lot of details on js code
      - fixed accordion changing size with artificial margin at bottom
      - added function to scroll to current page toc in sidebar on load
      - added function to close sidebar on anchor clicks on narrow screen

---
+++ global/sidebar.js
@@ -0,0 +1,56 @@
+
+window.addEventListener("load", loadside);
+
+function loadside() {
+	var sidenav = document.getElementById("sidenav");
+	sidenav.addEventListener("click", portraitClickCloseBar);
+	var cur = curlinkelem();
+	if (!cur) return
+	toggfold(cur);
+	var h = cur.offsetTop - sidenav.offsetTop - 128;
+	setTimeout( ()=>{
+		sidenav.scrollTo({top:h, behavior:"smooth"});
+	}, 333);
+}
+
+function portraitClickCloseBar(e) {
+	if (! window.matchMedia("(max-aspect-ratio: 1)").matches)
+		return; // only for portrait screen
+	if (! e.target.closest("a"))
+		return; // only for click on anchor links
+	toggside();
+}
+
+function curlinkelem(){
+	var url = window.location.href .split("#")[0];
+	var links = document.querySelectorAll(".foldable > a");
+	for (var i = 0; i < links.length; i++)
+		if (url == links[i].href)
+			return links[i].parentElement;
+}
+
+function nobubble(event) {
+	event.stopPropagation();
+}
+
+function toggside() {
+	document.getElementById("sidebar").classList.toggle('folded');
+	document.getElementById("main-container").classList.toggle('folded');
+}
+
+var inanim = false;
+
+function toggfold(elem) {
+	if (inanim) return;
+	inanim = true;
+	elem.classList.toggle('folded');
+	elem = elem.nextElementSibling;
+	var folded = elem.classList.toggle('folded');
+	var h = elem.scrollHeight;
+	var par = elem.parentElement;
+	var pad = parseFloat(window.getComputedStyle(par).fontSize);
+	if (par.classList.contains("sidelist"))
+		par.style.height = par.scrollHeight + (folded ? -h : h+pad) + "px";
+	elem.style.height = (folded ? 0 : h+pad ) + "px";
+	elem.addEventListener("transitionend", event => { inanim = false; });
+}
