21bb1a3ce65bbf21a430f0aa73751f48b33101da
atrosekta <atrosekta@proton.me>
2023-08-17 15:06:15 +0200

    added a check for links bad casing in verify.sh
    aside i also have another script not uploaded
    because i spent too much time on it lol.
    it have too much (useless) options,
    but one of them is to fix the casing of all
    the bad links... i'll add it when i cleaned it,
    probably next time i need to use it

--- verify.sh
+++ verify.sh
@@ -12,3 +12,16 @@ for file in "${files[@]}"; do
 	echo -e "\tneither stated in any other file"
 done
 
+printf "\nsearching for bad casing in links...\n"
+for file in content/*.md
+do
+	badlinks=$( grep -PnTo '#[-\w]+\s*\)' "$file" | grep "[A-Z]") \
+		|| continue
+	printf "%s\n\e[31m%s\e[0m\n" "$file" "$badlinks"
+	HASBADLINKS=1
+done
+
+[ -z $HASBADLINKS ] \
+	&& echo -e "  \e[32mfound none.\e[0m" \
+	|| echo -e "ERROR FOUND SOME !\e[0m"
+
